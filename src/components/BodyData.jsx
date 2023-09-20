51d858f48e3a249c3fb89630c640d65e5833b043
AvirukBasak <avirukbasak@gmail.com>
2023-09-20 22:22:08 +0530

    moved images to public

--- src/components/BodyData.jsx
+++ src/components/BodyData.jsx
@@ -1,6 +1,8 @@
+import printHelloWorld from '@/scripts/script';
 import styles from '@/styles/BodyData.module.css';
 
 export default function BodyData() {
+  printHelloWorld();
   return (
     <div className={styles.background}>
       <h1>Demo App</h1>
