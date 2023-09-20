df34abb15ab6f43c12af8cce9d1ea96e3bf96bfe
AvirukBasak <avirukbasak@gmail.com>
2023-09-20 22:01:28 +0530

    updated template

--- src/components/BodyData.jsx
+++ src/components/BodyData.jsx
@@ -1,8 +1,10 @@
+import styles from '@/styles/BodyData.module.css';
+
 export default function BodyData() {
   return (
-    <>
+    <div className={styles.background}>
       <h1>Demo App</h1>
       <p>This is a paragraph</p>
-    </>
+    </div>
   );
 }
